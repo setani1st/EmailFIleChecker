973361c37b0533287b35c633387225ebee111334
setani1st <setanijoao@gmail.com>
2024-12-17 08:57:26 -0300

    the path just changed so I changed the path too

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
 # エクセルファイルパス
-EXCEL_PATH = r'G:\共有ドライブ\Publico\6) 研修・講座 (Curso e Seminário)\c) 汎米研修\2025\4) 事務関係\2025年汎米研修　選考名簿　講師用.xlsx'
+EXCEL_PATH = r'G:\共有ドライブ\Publico\7) イベント (Evento)\6) 研修・講座 (Curso e Seminário)\c) 汎米研修\2025\4) 事務関係\2025年汎米研修　選考名簿　講師用.xlsx'
 #ベースフォルダー
-BASE_FOLDER = r'G:\共有ドライブ\Publico\6) 研修・講座 (Curso e Seminário)\c) 汎米研修\2025\3) 研修生\6 研修生書類'
+BASE_FOLDER = r'G:\共有ドライブ\Publico\7) イベント (Evento)\6) 研修・講座 (Curso e Seminário)\c) 汎米研修\2025\3) 研修生\6 研修生書類\certo'
